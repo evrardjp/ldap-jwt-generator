016a30f45a994f64d77c018ad476f1e2fc85db64
Jean-Philippe Evrard <open-source@a.spamming.party>
2026-02-26 19:21:18 +0100

    Cleanup last kubi remnants
    - Old API types
    - docs
    - Kubernetes deployments
    - Workflows

--- pkg/apis/cagip/register.go
+++
@@ -1,5 +0,0 @@
-package cagip
-
-const (
-	GroupName = "cagip.github.com"
-)
--- pkg/apis/cagip/v1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the cache v1 API group
-// +k8s:deepcopy-gen=package
-// +groupName=cagip.github.com
-package v1
